a1a4741dfb0ea6d28af238f58b66133a2b87bd99
Bindu Devidas <>
2022-07-28 10:00:22 +0000

    my first program

--- p0original.c
+++ p0original.c
@@ -1,1 +1,7 @@
  
+# include <stdio.h>
+int main (void)
+{
+  printf("MY NAME IS BINDU DEVIDAS\n");
+return 0;
+}
--- p1original.c
+++ p1original.c
@@ -1,2 +1,7 @@
-
+#include<studio.h>
+int input();
+{
+int add(int a, int b);
+  printf("enter two numbers\n")
+void output(int a, int b, int sum);
 }
