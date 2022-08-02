ad61dd981152ab184503d7bb4c68502b35dc71fc
Bindu Devidas <110161182+Bindu-Devidas0@users.noreply.github.com>
2022-08-02 03:43:25 +0000

    new program

--- p0original.c
+++ p0original.c
@@ -2,6 +2,8 @@
 # include <stdio.h>
 int main (void)
 {
-  printf("MY NAME IS BINDU DEVIDAS\n");
-return 0;
+  float base = 10.0, height= 20.5;
+  float area = 0.5 * base * height ;
+  printf("THE AREA OF THE TRIANGLE WITH BASE =%f and height= %f is %f \n",base,height,area);
+ return 0;
 }
